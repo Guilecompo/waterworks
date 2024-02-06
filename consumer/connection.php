4bb6aa5197c679d92331a172bae8824b0228b174
Guilecompo <kideguilecompo03@gmail.com>
2024-02-07 02:17:42 +0800

    Update connection.php

--- consumer/connection.php
+++ consumer/connection.php
@@ -1,5 +1,6 @@
 <?php
  header('Content-Type: application/json');
+ header("Access-Control-Allow-Origin: *");
  
  $host = 'bsyzrflyvtz0qvuxpqgm-mysql.services.clever-cloud.com';
  $dbusername = 'ua8azikuxiwmatlo';
