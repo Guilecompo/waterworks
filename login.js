4b23fc44c93cbd7bfc56729b4c612b1b4d9457d5
Guilecompo <kideguilecompo03@gmail.com>
2024-02-07 09:10:10 +0800

    a

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 const login = async () => {
-    const url = "http://sql6.freesqldatabase.com:3306/sql6682408/login.php";
+    const url = "http://sql6.freesqldatabase.com/login.php";
   
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
