f336033cbe90fb9edfde5ac4f14855331553d8cd
Guilecompo <kideguilecompo03@gmail.com>
2024-02-07 11:59:48 +0800

    Update consumer_billing_history1.php

--- consumer/consumer_billing_history1.php
+++ consumer/consumer_billing_history1.php
@@ -13,7 +13,7 @@ $stmt = $conn->prepare("SELECT
         c.user_id, c.meter_no,
         c.firstname AS con_firstname, c.middlename AS con_middlename, c.lastname AS con_lastname,
         d.zone_name, e.barangay_name,
-        f.municipality_name,
+        f.municipality_name, g.update_status_name,
         a.cubic_consumed,
         DATE_FORMAT(a.reading_date, '%m-%d') AS reading_date,
         DATE_FORMAT(a.due_date, '%m-%d') AS due_date,
@@ -31,6 +31,7 @@ $stmt = $conn->prepare("SELECT
     INNER JOIN address_zone d ON c.addressId = d.zone_id
     INNER JOIN address_barangay e ON d.barangayId = e.barangay_id
     INNER JOIN address_municipality f ON e.municipalityId = f.municipality_id
+    INNER JOIN update_status g ON a.billing_update_statusId  = g.update_status_id
     WHERE a.billing_id = :billing_id ORDER BY billing_id DESC LIMIT 1");
 
 $stmt->bindParam(":billing_id", $billing_id);
