ecb2ddd38b86219be1b1a48850950666b9d10f6a
KideGuile <kideguilecompo03@gmail.com>
2024-02-10 11:23:16 +0800

    A

--- login.js
+++ login.js
@@ -1,27 +1,31 @@
 const login = async () => {
-    const url = "http://localhost/waterworks/login.php";
-  
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-  
-    if (username === '' || password === '') {
-      alert('Fill in both username and password fields');
-    } else {
-      const formData = new FormData();
-      formData.append("username", username);
-      formData.append("password", password);
-  
-      try {
-        const response = await axios.post(url, formData);
-        //console.log(response);
-  
-        if (response.data.success) {
-          const { usertype, userDetails } = response.data;
-          console.log(response.data);
-  
-          // Store user information in sessionStorage
-          if (usertype === "Consumer") {
-            sessionStorage.setItem("fullname", userDetails.firstname + ' ' + userDetails.middlename + ' ' + userDetails.lastname);
+  const url = "http://localhost/waterworks/login.php";
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === '' || password === '') {
+    alert('Fill in both username and password fields');
+  } else {
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    try {
+      const response = await axios.post(url, formData);
+
+      if (response.data.success) {
+        const { usertype, userDetails } = response.data;
+
+        // Store user information in sessionStorage
+        sessionStorage.setItem("fullname", userDetails.firstname + ' ' + userDetails.lastname);
+        sessionStorage.setItem("phone_no", userDetails.phone_no);
+        sessionStorage.setItem("email", userDetails.email);
+        sessionStorage.setItem("accountId", userDetails.user_id);
+
+        // Redirect user to dashboard based on user type
+        switch (usertype) {
+          case "Consumer":
             sessionStorage.setItem("address", userDetails.zone_name + ', ' + userDetails.barangay_name + ', ' + userDetails.municipality_name);
             sessionStorage.setItem("location", userDetails.barangayId);
             sessionStorage.setItem("branchId", userDetails.branchId);
@@ -29,70 +33,64 @@ const login = async () => {
             sessionStorage.setItem("branchName", userDetails.branch_name);
             sessionStorage.setItem("positionName", userDetails.position_name);
             sessionStorage.setItem("propertyId", userDetails.propertyId);
-            sessionStorage.setItem("phone_no", userDetails.phone_no);
             sessionStorage.setItem("meter", userDetails.meter_no);
-            sessionStorage.setItem("email", userDetails.email);
-            sessionStorage.setItem("accountId", userDetails.user_id);
-
             window.location.href = "./consumer/html/dashboard.html";
-          } else {
-            // Assuming the default case is for employees
-            sessionStorage.setItem("fullname", userDetails.firstname + ' ' + userDetails.lastname);
+            break;
+          case "Admin":
             sessionStorage.setItem("address", userDetails.barangayName + ', ' + userDetails.municipalityName + ', ' + userDetails.provinceName);
             sessionStorage.setItem("branchId", userDetails.branchId);
             sessionStorage.setItem("branchName", userDetails.branch_name);
             sessionStorage.setItem("positionName", userDetails.position_name);
-            sessionStorage.setItem("phone_no", userDetails.phone_no);
             sessionStorage.setItem("usernames", userDetails.username);
-            sessionStorage.setItem("email", userDetails.email);
             sessionStorage.setItem("barangayId", userDetails.barangayId);
-            sessionStorage.setItem("accountId", userDetails.user_id);
-  
-            // Adjust the following cases based on your specific usertype values
-            switch (usertype) {
-              case "Admin":
-                window.location.href = "./admin/html/dashboard.html";
-                break;
-              case "Head":
-                window.location.href = "./head/html/dashboard.html";
-                break;
-              case "Clerk":
-                window.location.href = "./clerk/html/dashboard.html";
-                break;
-              case "Meter Reader":
-                window.location.href = "./meterreader/html/dashboard.html";
-                break;
-  
-              default:
-                alert("Unknown usertype");
-            }
-          }
-        } else {
-          alert("Login Failed: " + response.data.message);
-          console.log(response.data);
+            window.location.href = "./admin/html/dashboard.html";
+            break;
+          case "Head":
+            sessionStorage.setItem("branchId", userDetails.branchId);
+            sessionStorage.setItem("branchName", userDetails.branch_name);
+            sessionStorage.setItem("positionName", userDetails.position_name);
+            window.location.href = "./head/html/dashboard.html";
+            break;
+          case "Clerk":
+            sessionStorage.setItem("branchId", userDetails.branchId);
+            sessionStorage.setItem("branchName", userDetails.branch_name);
+            sessionStorage.setItem("positionName", userDetails.position_name);
+            window.location.href = "./clerk/html/dashboard.html";
+            break;
+          case "Meter Reader":
+            sessionStorage.setItem("branchId", userDetails.branchId);
+            sessionStorage.setItem("branchName", userDetails.branch_name);
+            sessionStorage.setItem("positionName", userDetails.position_name);
+            window.location.href = "./meterreader/html/dashboard.html";
+            break;
+          default:
+            alert("Unknown usertype");
         }
-      } catch (error) {
-        alert("ERROR! - " + error.message);
+      } else {
+        alert("Login Failed: " + response.data.message);
+        console.log(response.data);
       }
-    }
-  };
-  
-  const inputs = document.querySelectorAll(".input");
-  
-  function addClass() {
-    let parent = this.parentNode.parentNode;
-    parent.classList.add("focus");
-  }
-  
-  function removeClass() {
-    let parent = this.parentNode.parentNode;
-    if (this.value === "") {
-      parent.classList.remove("focus");
+    } catch (error) {
+      alert("ERROR! - " + error.message);
     }
   }
-  
-  inputs.forEach(input => {
-    input.addEventListener("focus", addClass);
-    input.addEventListener("blur", removeClass);
-  });
-  
+};
+
+const inputs = document.querySelectorAll(".input");
+
+function addClass() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
+
+function removeClass() {
+  let parent = this.parentNode.parentNode;
+  if (this.value === "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener("focus", addClass);
+  input.addEventListener("blur", removeClass);
+});
