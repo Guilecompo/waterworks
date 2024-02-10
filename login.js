73240e1b3319c64994fb961b1d4b75fa2c48f003
KideGuile <kideguilecompo03@gmail.com>
2024-02-10 11:35:40 +0800

    A

--- login.js
+++ login.js
@@ -13,58 +13,59 @@ const login = async () => {
 
     try {
       const response = await axios.post(url, formData);
+      //console.log(response);
 
       if (response.data.success) {
         const { usertype, userDetails } = response.data;
+        console.log(response.data);
 
         // Store user information in sessionStorage
-        sessionStorage.setItem("fullname", userDetails.firstname + ' ' + userDetails.lastname);
-        sessionStorage.setItem("phone_no", userDetails.phone_no);
-        sessionStorage.setItem("email", userDetails.email);
-        sessionStorage.setItem("accountId", userDetails.user_id);
+        if (usertype === "Consumer") {
+          sessionStorage.setItem("fullname", userDetails.firstname + ' ' + userDetails.middlename + ' ' + userDetails.lastname);
+          sessionStorage.setItem("address", userDetails.zone_name + ', ' + userDetails.barangay_name + ', ' + userDetails.municipality_name);
+          sessionStorage.setItem("location", userDetails.barangayId);
+          sessionStorage.setItem("branchId", userDetails.branchId);
+          sessionStorage.setItem("barangayId", userDetails.barangay_id);
+          sessionStorage.setItem("branchName", userDetails.branch_name);
+          sessionStorage.setItem("positionName", userDetails.position_name);
+          sessionStorage.setItem("propertyId", userDetails.propertyId);
+          sessionStorage.setItem("phone_no", userDetails.phone_no);
+          sessionStorage.setItem("meter", userDetails.meter_no);
+          sessionStorage.setItem("email", userDetails.email);
+          sessionStorage.setItem("accountId", userDetails.user_id);
 
-        // Redirect user to dashboard based on user type
-        switch (usertype) {
-          case "Consumer":
-            sessionStorage.setItem("address", userDetails.zone_name + ', ' + userDetails.barangay_name + ', ' + userDetails.municipality_name);
-            sessionStorage.setItem("location", userDetails.barangayId);
-            sessionStorage.setItem("branchId", userDetails.branchId);
-            sessionStorage.setItem("barangayId", userDetails.barangay_id);
-            sessionStorage.setItem("branchName", userDetails.branch_name);
-            sessionStorage.setItem("positionName", userDetails.position_name);
-            sessionStorage.setItem("propertyId", userDetails.propertyId);
-            sessionStorage.setItem("meter", userDetails.meter_no);
-            window.location.href = "./consumer/html/dashboard.html";
-            break;
-          case "Admin":
-            sessionStorage.setItem("address", userDetails.barangayName + ', ' + userDetails.municipalityName + ', ' + userDetails.provinceName);
-            sessionStorage.setItem("branchId", userDetails.branchId);
-            sessionStorage.setItem("branchName", userDetails.branch_name);
-            sessionStorage.setItem("positionName", userDetails.position_name);
-            sessionStorage.setItem("usernames", userDetails.username);
-            sessionStorage.setItem("barangayId", userDetails.barangayId);
-            window.location.href = "./admin/html/dashboard.html";
-            break;
-          case "Head":
-            sessionStorage.setItem("branchId", userDetails.branchId);
-            sessionStorage.setItem("branchName", userDetails.branch_name);
-            sessionStorage.setItem("positionName", userDetails.position_name);
-            window.location.href = "./head/html/dashboard.html";
-            break;
-          case "Clerk":
-            sessionStorage.setItem("branchId", userDetails.branchId);
-            sessionStorage.setItem("branchName", userDetails.branch_name);
-            sessionStorage.setItem("positionName", userDetails.position_name);
-            window.location.href = "./clerk/html/dashboard.html";
-            break;
-          case "Meter Reader":
-            sessionStorage.setItem("branchId", userDetails.branchId);
-            sessionStorage.setItem("branchName", userDetails.branch_name);
-            sessionStorage.setItem("positionName", userDetails.position_name);
-            window.location.href = "./meterreader/html/dashboard.html";
-            break;
-          default:
-            alert("Unknown usertype");
+          window.location.href = "./consumer/html/dashboard.html";
+        } else {
+          // Assuming the default case is for employees
+          sessionStorage.setItem("fullname", userDetails.firstname + ' ' + userDetails.lastname);
+          sessionStorage.setItem("address", userDetails.barangayName + ', ' + userDetails.municipalityName + ', ' + userDetails.provinceName);
+          sessionStorage.setItem("branchId", userDetails.branchId);
+          sessionStorage.setItem("branchName", userDetails.branch_name);
+          sessionStorage.setItem("positionName", userDetails.position_name);
+          sessionStorage.setItem("phone_no", userDetails.phone_no);
+          sessionStorage.setItem("usernames", userDetails.username);
+          sessionStorage.setItem("email", userDetails.email);
+          sessionStorage.setItem("barangayId", userDetails.barangayId);
+          sessionStorage.setItem("accountId", userDetails.user_id);
+
+          // Adjust the following cases based on your specific usertype values
+          switch (usertype) {
+            case "Admin":
+              window.location.href = "./admin/html/dashboard.html";
+              break;
+            case "Head":
+              window.location.href = "./head/html/dashboard.html";
+              break;
+            case "Clerk":
+              window.location.href = "./clerk/html/dashboard.html";
+              break;
+            case "Meter Reader":
+              window.location.href = "./meterreader/html/dashboard.html";
+              break;
+
+            default:
+              alert("Unknown usertype");
+          }
         }
       } else {
         alert("Login Failed: " + response.data.message);
