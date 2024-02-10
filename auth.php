936de4d820928c625546fd65f60ecbe53be292c5
KideGuile <kideguilecompo03@gmail.com>
2024-02-10 13:42:27 +0800

    2

--- auth.php
+++ auth.php
@@ -3,7 +3,7 @@ session_start(); // Initialize the session
 
 // Check if the user is not logged in, redirect to login page
 if (!isset($_SESSION['loggedIn'])) {
-    header("Location: login.html"); // Change this to your login page
+    header("Location: /"); // Change this to your login page
     exit();
 }
 ?>
