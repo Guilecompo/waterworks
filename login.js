5bd7b0c1af6a791067732101c4922e728e4aa6c7
KideGuile <kideguilecompo03@gmail.com>
2024-03-07 00:28:15 +0800

    a

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
   const login = async () => {
-      const url = "http://localhost/waterworks/login.php";
+      const url = "http://localhost/waterworks.github.com/login.php";
     
       const username = document.getElementById("username").value;
       const password = document.getElementById("password").value;
