70b5c5667d9e2cee6d22fcbcdc89d0c74fe76266
KideGuile <kideguilecompo03@gmail.com>
2024-03-14 12:14:38 +0800

    a

--- head/get_consumers_mulogan.php
+++ head/get_consumers_mulogan.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt = $conn->prepare("SELECT 
             a.user_id,a.meter_no,
             a.firstname, a.middlename,
-            a.lastname, 
+            a.lastname, a.connected_number,
             a.phone_no, c.property_name,
             d.zone_name, e.barangay_name,
             f.municipality_name, a.meter_no,
