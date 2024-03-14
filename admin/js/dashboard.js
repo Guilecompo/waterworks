50403cfdfd47336807576b6ba54a42397bc0425d
KideGuile <kideguilecompo03@gmail.com>
2024-03-14 10:08:30 +0800

    a

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -84,6 +84,7 @@ const getpoblacion = (selectedBranch) => {
   })
   .then(response => response.data)  // Corrected line
   .then(data => {
+    console.log(response.data);
     console.log('Response data:', data); // Log the response
     if (data && data.Total_Consumers !== undefined 
         && data.Total_Employees !== undefined 
@@ -110,6 +111,7 @@ const getpoblacion = (selectedBranch) => {
 
   .catch(error => {
       console.error('Error fetching data:', error);
+      console.log(response.data);
   });
 }
 const getmolugan = () => {
