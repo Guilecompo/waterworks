73ffc22a9016fe266c69a85cd72cad8962dc70df
KideGuile <kideguilecompo03@gmail.com>
2024-03-14 10:18:15 +0800

    a

--- admin/total_poblacion.php
+++ admin/total_poblacion.php
@@ -10,25 +10,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Prepare and execute the SQL queries
         // total consumers
         $barangayId = $_POST['branchId'];
-        $stmt = $conn->prepare("SELECT COUNT(user_consumer.user_id) AS Total_Consumers FROM user_consumer INNER JOIN branch ON user_consumer.branchId = branch.branch_id WHERE branch.branch_name = ::barangayId");
+        $stmt = $conn->prepare("SELECT COUNT(user_consumer.user_id) AS Total_Consumers FROM user_consumer INNER JOIN branch ON user_consumer.branchId = branch.branch_id WHERE branch.branch_name = :barangayId");
         // $stmt = $conn->prepare("SELECT COUNT(user_id) AS Total_Consumers FROM user_consumer WHERE branchId = 1");
         $stmt->bindParam(":barangayId", $barangayId);
         $stmt->execute();
         $CTotalresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         // total employees
-        $stmt = $conn->prepare("SELECT COUNT(user_employee.user_id) AS Total_Employees FROM user_employee INNER JOIN branch ON user_employee.branchId = branch.branch_id WHERE branch.branch_name = ::barangayId");
+        $stmt = $conn->prepare("SELECT COUNT(user_employee.user_id) AS Total_Employees FROM user_employee INNER JOIN branch ON user_employee.branchId = branch.branch_id WHERE branch.branch_name = :barangayId");
         $stmt->bindParam(":barangayId", $barangayId);
         $stmt->execute();
         $ETotalresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         // total consumed (pres + prev)
-        $stmt = $conn->prepare("SELECT SUM(billing.cubic_consumed) AS Total_Consumed FROM billing INNER JOIN branch ON billing.branchId = branch.branch_id WHERE branch.branch_name = ::barangayId");
+        $stmt = $conn->prepare("SELECT SUM(billing.cubic_consumed) AS Total_Consumed FROM billing INNER JOIN branch ON billing.branchId = branch.branch_id WHERE branch.branch_name = :barangayId");
         $stmt->bindParam(":barangayId", $barangayId);
         $stmt->execute();
         $PresConsumedTotalresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        $stmt = $conn->prepare("SELECT SUM(payment.pay_amount) AS Total_Pay FROM payment INNER JOIN branch ON payment.branchId = branch.branch_id WHERE branch.branch_name = ::barangayId");
+        $stmt = $conn->prepare("SELECT SUM(payment.pay_amount) AS Total_Pay FROM payment INNER JOIN branch ON payment.branchId = branch.branch_id WHERE branch.branch_name = :barangayId");
         $stmt->bindParam(":barangayId", $barangayId);
         $stmt->execute();
         $PayTotalresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
