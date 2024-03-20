0cc6493c5bed25e68d56274920daeb955de31f6c
KideGuile <kideguilecompo03@gmail.com>
2024-03-21 07:19:34 +0800

    a

--- admin/html/set_session.php
+++ admin/html/set_session.php
@@ -3,7 +3,7 @@
 session_start();
 
 // Check if accountId is sent via POST
-if(!isset($_POST['accountId']) || $_SESSION['accountId'] == 0 || empty($_SESSION['accountId'])) {
+if($_SESSION['accountId'] == 0 || empty($_SESSION['accountId'])) {
     // Set accountId in session
     http_response_code(400);
     echo "Error: Account ID not provided.";
