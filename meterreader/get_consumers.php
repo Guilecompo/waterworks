e6b5f4bbdc176b49cf8deff8a34e050418d730d1
KideGuile <kideguilecompo03@gmail.com>
2024-03-22 14:09:06 +0800

    a

--- meterreader/get_consumers.php
+++ meterreader/get_consumers.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $currentDay = date('j');
 
         // Check if the current day is between 25th and 5th
-        if ($currentDay < 25 && $currentDay > 5) {
+        if ($currentDay < 22 && $currentDay > 5) {
             echo json_encode(["error" => "Data can only be displayed between the 25th and 5th of the month."]);
             exit; // Stop further execution
         }
