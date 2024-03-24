7d8b6b9f682554a29b6d8ed0842b6000e1162e02
KideGuile <kideguilecompo03@gmail.com>
2024-03-24 17:36:05 +0800

    a

--- z_forgot/change_pass.js
+++ z_forgot/change_pass.js
@@ -98,16 +98,16 @@ var html = `
     const modal = document.getElementById("myModal");
     const modalContent = document.getElementById("modalContent");
     var html = `
-        <div style="background-color: #fff; border-radius: 10px; padding: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-            <h3 style="color: #FF5733; text-align: center; margin-bottom: 10px;">Password Requirements</h3>
-            <ul style="list-style-type: none; padding-left: 0; margin-bottom: 0;">
-                <li style="color: #FF5733; margin-bottom: 5px;"><strong>Be 8 to 12 characters long</strong></li>
-                <li style="color: #FF5733; margin-bottom: 5px;"><strong>Contain at least one uppercase letter</strong></li>
-                <li style="color: #FF5733; margin-bottom: 5px;"><strong>Contain at least one lowercase letter</strong></li>
-                <li style="color: #FF5733; margin-bottom: 5px;"><strong>Contain at least one digit (0-9)</strong></li>
-                <li style="color: #FF5733; margin-bottom: 5px;"><strong>Contain at least one special character (!@#$%^&* etc.)</strong></li>
-            </ul>
-        </div>
+    <div style="background-color: #fff; border-radius: 10px; padding: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+    <h3 style="color: #FF5733; text-align: center; margin-bottom: 10px;">Password Requirements</h3>
+    <ul style="list-style-type: none; padding-left: 0; margin-bottom: 0;">
+      <li style="color: #FF5733; margin-bottom: 5px;"><strong>Be 8 to 12 characters long</strong></li>
+      <li style="color: #FF5733; margin-bottom: 5px;"><strong>Contain at least one uppercase letter</strong></li>
+      <li style="color: #FF5733; margin-bottom: 5px;"><strong>Contain at least one lowercase letter</strong></li>
+      <li style="color: #FF5733; margin-bottom: 5px;"><strong>Contain at least one digit (0-9)</strong></li>
+      <li style="color: #FF5733; margin-bottom: 5px;"><strong>Contain at least one special character (!@#$%^&* etc.)</strong></li>
+    </ul>
+  </div>
     `;
     modalContent.innerHTML = html;
     modal.style.display = "block";
