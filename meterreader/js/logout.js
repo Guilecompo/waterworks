1ec40f5bb84d76cde55d125d9bd8ea1f2ed9b16e
KideGuile <kideguilecompo03@gmail.com>
2024-03-24 11:28:31 +0800

    a

---
+++ meterreader/js/logout.js
@@ -0,0 +1,26 @@
+// Define the logout function
+const logout = async () => {
+    try {
+      const response = await fetch('http://128.199.232.132/waterworks/gets/logout.php', {
+        method: 'POST'
+      });
+  
+      if (response.ok) {
+        // Redirect to the login page after successful logout
+        sessionStorage.clear();
+        window.location.href = "/waterworks/"; // Replace with your login page URL
+        console.log('logout success');
+      } else {
+        console.error('Logout failed');
+      }
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  };
+  
+  // Attach the logout function to the logout link
+  document.getElementById("logoutBtn").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    logout(); // Call the logout function when the link is clicked
+  });
+  
