0f75fd3cf874b37a2bfe65490ff8e4bf2e78d7c7
Guile <guilecompo26@gmail.com>
2024-07-15 14:42:47 +0800

    a

--- connection.php
+++ connection.php
@@ -1,20 +1,22 @@
 <?php
-header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+ header('Content-Type: application/json');
+ header("Access-Control-Allow-Origin: *");
+ 
+//  $host = 'sql303.infinityfree.com';
+//  $dbusername = 'if0_36105237';
+//  $dbpassword = 'S2gyyhJcalFV4';
+//  $database = 'if0_36105237_waterbilling';
 
-
-  $host = '152.42.243.189';
-  $dbusername = 'waterworks';
-  $dbpassword = 'Waterworks2024!';
-  $database = 'waterbilling';
+$host = '152.42.243.189';
+ $dbusername = 'waterworks';
+ $dbpassword = 'Waterworks2024!';
+ $database = 'waterbilling';
 
   try {
     $conn = new PDO("mysql:host=$host;dbname=$database", $dbusername, $dbpassword);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connection Successful!";
+    //echo "Connection Successful!";
   } catch(PDOException $e) {
     die("Error: " . $e->getMessage());
   }
