21846bdd03c2074af4feca084294c14ca97dba90
Guile Compo <kideguilecompo03@gmail.com>
2024-09-18 16:20:03 +0800

    asda

--- meterreader/get_zones_filter.php
+++ meterreader/get_zones_filter.php
@@ -2,19 +2,12 @@
 header("Access-Control-Allow-Origin: *");
 include 'connection.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Log the request method
-error_log("Request Method: " . $_SERVER['REQUEST_METHOD']);
-
 // Assuming you're using POST method to send data
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Validate if 'readerId' and 'barangayId' are set
     if (isset($_POST['readerId'], $_POST['branchId'])) {
         $readerId = $_POST['readerId'];
-        $barangayId = $_POST['branchId'];
+        $branchId = $_POST['branchId'];
 
         // Check if 'readerId' exists in the 'assign' table
         $stmt = $conn->prepare("SELECT * FROM assign WHERE emp_Id = :readerId");
@@ -31,13 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     FROM address_zone a
                     INNER JOIN address_barangay b ON a.barangayId = b.barangay_id
                     WHERE a.barangayId = :branchId AND a.zone_id IN (".implode(',', $zoneIds).")
-                    ORDER BY a.zone_name";
-            // $sql = "SELECT b.zone_id, b.zone_name, c.barangay_name
-            //         FROM branch a
-            //         INNER JOIN address_zone b ON a.locationId = b.zone_id
-            //         INNER JOIN address_barangay c ON b.barangayId = c.barangay_id
-            //         WHERE a.branch_id = :branchId AND b.zone_id IN (".implode(',', $zoneIds).")
-            //         ORDER BY b.zone_name";
+                    ORDER BY zone_name";
 
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(":branchId", $branchId);
