4cccfffb857374b52927f95c83150ac5f10c0bef
Guile Compo <kideguilecompo03@gmail.com>
2024-09-18 14:29:51 +0800

    zxc

--- admin/js/reader.js
+++ admin/js/reader.js
@@ -570,7 +570,7 @@ const view_assigned = (user_id) => {
                       <tr>
                           <td>${record.zone_name}</td>
                           <td>${record.branch_name}</td>
-                          <button class="butt" onclick="view_assigned(${employee.user_id})">Remove</button>
+                          <button class="butt" onclick="view_assigned(${record.user_id})">Remove</button>
                       </tr>
                   `;
           });
