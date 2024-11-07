6c9e546c69a9eef8f8fa54f28444163b0dd09a0b
Guilecompo <117016354+Guilecompo@users.noreply.github.com>
2024-11-07 11:27:28 +0800

    xzc

--- admin/js/consumer.js
+++ admin/js/consumer.js
@@ -102,7 +102,6 @@ const consumerRefreshTables = (consumers) => {
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown1">
                         <li><a class="dropdown-item" onclick="change(${consumer.user_id})">Change Meter</a></li>
                         <li><a class="dropdown-item" onclick="add(${consumer.user_id})">Add Connected</a></li>
-                        <li><a class="dropdown-item" onclick="more(${consumer.user_id})">View Connected</a></li>
                     </ul>
               </td>
             </tr>
